2e48331a181b4acdf7afe946bf1c6e3fbce017c7
juninhoww2 <alqadianoKira@gmail.com>
2020-11-27 19:18:26 -0300

    update files

--- knexfile.js
+++ knexfile.js
@@ -9,6 +9,49 @@ module.exports = {
         migrations: {
             directory: "./src/database/migrations"
         },
-        useNull
+        useNullAsDefault: true
+    },
+
+    test: {
+        client: "sqlite3",
+        connection: {
+            test: "./src/database/test.sqlite"
+        },
+        migrations: {
+            directory: "./src/database/migrations"
+        },
+        useNullAsDefault: true
+    },
+
+    staging: {
+        client: "postgresql",
+        connection: {
+            database: "my_db",
+            user: "username",
+            password: "password"
+        },
+        pool: {
+            min: 2,
+            max: 10
+        },
+        migrations: {
+            tableName: "knex_migrations"
+        }
+    },
+
+    production: {
+        client: "postgresql",
+        connection: {
+            database: "my_db",
+            user: "username",
+            password: "password"
+        },
+        pool: {
+            min: 2,
+            max: 10
+        },
+        migrations: {
+            tableName: "knex_migrations"
+        }
     }
-}
+};
---
+++ src/routes.js
@@ -0,0 +1,6 @@
+const express = require('express')
+
+const OngController = require('')
+const IncidentController = require('')
+const ProfileController = require('')
+const SessionController = require('')
